9a5ec2c29837596d58816bd62530911a8da98715
Nijat Mursali <n.mursali@mail.ru>
2020-02-23 23:08:41 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AIROSHRIPROJECT
 This is the repository made for SHRI (Spoken Human Robot Interaction) for the Artificial Intelligence course in Sapienza University of Rome, Italy.
+
+## Introduction
+
+## How Speech Recognition Works
---
+++ main.py
@@ -0,0 +1,18 @@
+''' Import Libraries '''
+
+import speech_recognition as sr
+import os
+import spacy
+import nltk
+from nltk import Tree
+from spacy import displacy
+from spacy.symbols import NOUN, NUM
+
+''' See Versions '''
+print(sr.__version__)
+print(spacy.__version__)
+print(nltk.__version__)
+
+''' Next Steps '''
+
+print('')
